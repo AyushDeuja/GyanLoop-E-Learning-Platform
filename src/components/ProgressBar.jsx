5a943f00d84ebfbf7c5223faf135d48052f00530
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-28 18:42:18 +0545

    created progress bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const ProgressBar = () => {
-  return <div></div>;
+const ProgressBar = ({ progress }) => {
+  return (
+    <div className="w-full bg-gray-700 rounded-full h-2.5">
+      <div
+        className="bg-blue-500 h-2.5 rounded-full"
+        style={{ width: `${progress}%` }}
+      ></div>
+    </div>
+  );
 };
 
 export default ProgressBar;
