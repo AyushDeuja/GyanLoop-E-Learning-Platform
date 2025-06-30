3845b98deb5abdf21be1bad5465fd206e207dc3d
AyushDeuja <ayushdeuja11@gmail.com>
2025-06-30 18:03:58 +0545

    validation done in signup using yup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -8,6 +8,14 @@ import { LOGO_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { login } from "../redux/authSlice";
 import { LucideArrowLeft } from "lucide-react";
+import { object, string } from "yup";
+
+const signUpSchema = object({
+  name: string().required("Name is required"),
+  email: string().email("Invalid email format").required("Email is required"),
+  mobile: string().required("Mobile is required"),
+  password: string().min(6, "Password must be at least 6 characters"),
+});
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -18,16 +26,25 @@ const SignUp = () => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const values = Object.fromEntries(formData.entries());
+
     try {
-      const response = await axiosInstance.post(`/auth/register`, values);
+      const validated = await signUpSchema.validate(values, {
+        abortEarly: false,
+      });
+
+      const response = await axiosInstance.post(`/auth/register`, validated);
       dispatch(login(response.data.token));
       navigate("/courses");
       toast.success("Welcome");
     } catch (err) {
-      setErrorMessage(
-        err.response?.data?.message || "Registration failed, Please try again"
-      );
-      toast.error("Registration failed, Please try again");
+      if (err.name === "ValidationError") {
+        setErrorMessage(err.errors.join(" | "));
+      } else {
+        setErrorMessage(
+          err.response?.data?.message || "Registration failed, Please try again"
+        );
+        toast.error("Registration failed, Please try again");
+      }
     }
   };
 
@@ -38,12 +55,15 @@ const SignUp = () => {
       </div>
       <div className="bg-white p-8 rounded-2xl shadow-xl w-full max-w-md">
         <div className="flex items-center justify-between mb-6">
-          <LucideArrowLeft className="h-6 w-6" onClick={() => navigate("/")} />
+          <LucideArrowLeft
+            className="h-6 w-6 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
           <h1 className="text-2xl font-bold text-gray-800">Sign Up</h1>
         </div>
         <form className="space-y-4" onSubmit={handleSubmit}>
           <CustomInput name="name" type="text" id="name" label="Name" />
-          <CustomInput name="email" type="email" id="email" label="Email" />
+          <CustomInput name="email" type="text" id="email" label="Email" />
           <CustomInput name="mobile" type="tel" id="mobile" label="Mobile" />
           <CustomInput
             name="password"
