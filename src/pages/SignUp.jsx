216fa3301a57ac31214eb3fe1a768714912b070d
AyushDeuja <ayushdeuja11@gmail.com>
2025-07-05 11:15:26 +0545

    made signup page responsive

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,19 +2,20 @@ import { NavLink, useNavigate } from "react-router";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import { axiosInstance } from "../utils/axiosInterceptor";
-import CustomButton from "../components/CustomButton";
 import CustomInput from "../components/CustomInput";
+import CustomButton from "../components/CustomButton";
 import { LOGO_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { login } from "../redux/authSlice";
 import { LucideArrowLeft } from "lucide-react";
 import { object, string } from "yup";
 
-const signUpSchema = object({
+const signupSchema = object({
   name: string().required("Name is required"),
-  email: string().email("Invalid email format").required("Email is required"),
-  mobile: string().required("Mobile is required"),
-  password: string().min(6, "Password must be at least 6 characters"),
+  username: string().required("Username is required"),
+  password: string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
 });
 
 const SignUp = () => {
@@ -28,43 +29,50 @@ const SignUp = () => {
     const values = Object.fromEntries(formData.entries());
 
     try {
-      const validated = await signUpSchema.validate(values, {
+      const validated = await signupSchema.validate(values, {
         abortEarly: false,
       });
 
-      const response = await axiosInstance.post(`/auth/register`, validated);
+      const response = await axiosInstance.post(`/auth/signup`, validated);
       dispatch(login(response.data.token));
       navigate("/dashboard");
-      toast.success("Welcome");
+      toast.success("Account created successfully");
     } catch (err) {
       if (err.name === "ValidationError") {
         setErrorMessage(err.errors.join(" | "));
       } else {
         setErrorMessage(
-          err.response?.data?.message || "Registration failed, Please try again"
+          err.response?.data?.message || "Signup failed, please try again"
         );
-        toast.error("Registration failed, Please try again");
+        toast.error("Signup failed, please try again");
       }
     }
   };
 
   return (
-    <div className="min-h-screen flex gap-50 flex-row items-center justify-center bg-gradient-to-b from-gray-800 to-gray-900">
-      <div>
-        <img src={LOGO_URL} alt="Logo" />
+    <div className="min-h-screen flex flex-col md:flex-row items-center justify-center bg-gradient-to-b from-gray-800 to-gray-900 px-4 py-10">
+      <div className="mb-8 md:mb-0 md:mr-40">
+        <img src={LOGO_URL} alt="Logo" className="w-48 md:w-100 mx-auto" />
       </div>
-      <div className="bg-white p-8 rounded-2xl shadow-xl w-full max-w-md">
+
+      <div className="bg-white w-full max-w-md p-6 sm:p-8 rounded-2xl shadow-xl">
         <div className="flex items-center justify-between mb-6">
           <LucideArrowLeft
             className="h-6 w-6 cursor-pointer"
             onClick={() => navigate("/")}
           />
-          <h1 className="text-2xl font-bold text-gray-800">Sign Up</h1>
+          <h1 className="text-xl sm:text-2xl font-bold text-gray-800">
+            Sign Up
+          </h1>
         </div>
         <form className="space-y-4" onSubmit={handleSubmit}>
-          <CustomInput name="name" type="text" id="name" label="Name" />
-          <CustomInput name="email" type="text" id="email" label="Email" />
-          <CustomInput name="mobile" type="tel" id="mobile" label="Mobile" />
+          <CustomInput name="name" type="text" id="name" label="Full Name" />
+          <CustomInput
+            name="username"
+            type="text"
+            id="username"
+            label="Email or Mobile"
+          />
           <CustomInput
             name="password"
             type="password"
@@ -76,10 +84,10 @@ const SignUp = () => {
           )}
           <CustomButton label="Sign Up" type="submit" />
         </form>
-        <p className="text-md text-center text-gray-800 mt-6">
+        <p className="text-sm sm:text-md text-center text-gray-800 mt-6">
           Already have an account?{" "}
           <NavLink to="/login" className="text-blue-600 hover:underline">
-            Log in
+            Login
           </NavLink>
         </p>
       </div>
